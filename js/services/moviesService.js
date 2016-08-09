17214dab72e8d2fdbc8c276589deb9b6eb1c3e45
Ray Walters <rlwalters3@yahoo.com>
2016-08-09 14:46:44 -0400

    pulled new api

--- js/services/moviesService.js
+++ js/services/moviesService.js
@@ -5,7 +5,7 @@ var app = angular.module('entertainApp');
 app.factory('movieList',['$http', function($http){
 	return{
 		data: function(){
-			return $http.get("https://api.themoviedb.org/3/genre/28/movies?api_key=f26d8020ab7996e8ae35fa0d465c6732");
+			return $http.get("http://api.themoviedb.org/3/discover/movie?api_key=f26d8020ab7996e8ae35fa0d465c6732&with_genres=12");
 		}
 	};
 	// $http({
