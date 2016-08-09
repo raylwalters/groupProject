4f04286119c720eb36b9652dfb8c9ca068fedd95
Katie Golicz <goliczk@gmail.com>
2016-08-09 14:39:41 -0400

    updated book stuff

--- js/controllers/booksCtrl.js
+++ js/controllers/booksCtrl.js
@@ -2,17 +2,22 @@
 
 var app = angular.module('entertainApp');
 
-app.controller('booksCtrl', function($scope,$http) {
+
+app.controller('booksCtrl', function($scope, $http) {
 	$http ({
 		method: 'GET',
-		url: 'https://www.googleapis.com/books/v1/volumes?q=search+terms:jane+austen&key=AIzaSyBYgMe4rhHaREeA8Z38sDKe-_iOJrIo--w'
+		url: 'https://www.googleapis.com/books/v1/volumes?q=search+terms:comedy&key=AIzaSyBYgMe4rhHaREeA8Z38sDKe-_iOJrIo--w'
 	}).then(function successCallback(response) {
-		console.log(response.data.items[0].volumeInfo.authors);
-		$scope.id=response.data.items[0].volumeInfo.authors;
-	}, function errorCallback(response) {
-		console.log('oops');	
-	});
+		var storyArray = [];
+		for(var i=0;i<5;i++){
+			storyArray.push(i);
+		}
+		$scope.storyArray = response.data.items;
+	
+	});	
 });
+	
+	
 
 
 /*var app = angular.module('myModule');
