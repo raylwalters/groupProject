5e276f7bdb91d2ec3889443d0d9361e08335fb2b
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-09 15:42:00 -0400

    deleted possible security breach

--- js/controllers/booksCtrl.js
+++ js/controllers/booksCtrl.js
@@ -6,18 +6,18 @@ var app = angular.module('entertainApp');
 app.controller('booksCtrl', function($scope, $http) {
 	$http ({
 		method: 'GET',
-		url: 'https://www.googleapis.com/books/v1/volumes?q=search+terms:comedy&key=AIzaSyBYgMe4rhHaREeA8Z38sDKe-_iOJrIo--w'
+		url: 'https://www.googleapis.com/books/v1/volumes?q=search+terms:comedy&key='
 	}).then(function successCallback(response) {
 		var storyArray = [];
 		for(var i=0;i<5;i++){
 			storyArray.push(i);
 		}
 		$scope.storyArray = response.data.items;
-	
-	});	
+
+	});
 });
-	
-	
+
+
 
 
 /*var app = angular.module('myModule');
@@ -30,6 +30,6 @@ app.controller('myController', function($scope,$http) {
 		console.log(response.data.items[0].volumeInfo.authors);
 		$scope.id=response.data.items[0].volumeInfo.authors;
 	}, function errorCallback(response) {
-		console.log('oops');	
+		console.log('oops');
 	});
 });*/
