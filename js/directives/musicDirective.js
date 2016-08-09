1e4e6575ffd09ac101dcb22280935266b8853fe8
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-09 14:37:04 -0400

    added spotify to music.html partial

--- js/directives/musicDirective.js
+++ js/directives/musicDirective.js
@@ -1,11 +1,11 @@
-'use strict';
-
-var app = angular.module('entertainApp');
-
-app.directive('musicDir',function(){
-	return{
-		restrict: 'AE',
-		templateUrl: 'partials/music.html',
-		replace: false
-	};
-});
+// 'use strict';
+//
+// var app = angular.module('entertainApp');
+//
+// app.directive('musicDir',function(){
+// 	return{
+// 		restrict: 'AE',
+// 		templateUrl: 'partials/music.html',
+// 		replace: false
+// 	};
+// });
