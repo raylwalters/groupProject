c5d2c6e5fb76c3b84faaa161f3d66a527adb19cf
Ray Walters <rlwalters3@yahoo.com>
2016-08-09 23:02:45 -0400

    Can't get individual images for each movie title

--- js/controllers/moviesCtrl.js
+++ js/controllers/moviesCtrl.js
@@ -6,10 +6,20 @@ app.controller('moviesCtrl',['$scope','movieList', function($scope,movieList){
 	movieList.data().then(function(response){
 		console.log(response);
 		$scope.movies = response.data.results;
+		// $scope.imageId1 = response.data.results[0].poster_path;
+		// $scope.image1 = "http://image.tmdb.org/t/p/w92/"+$scope.imageId1;
+		// $scope.imageId2 = response.data.results[1].poster_path;
+		// $scope.image2 = "http://image.tmdb.org/t/p/w92/"+$scope.imageId2;
+
+
 		$scope.movies.forEach(function(item,index){
 			$scope.imageId = response.data.results[index].poster_path;
 			$scope.image = "http://image.tmdb.org/t/p/w92/"+$scope.imageId;
+			// $scope.image = "http://image.tmdb.org/t/p/w92/";
+			
+			
 		});
+		
 		
 		console.log($scope.imageId);
 		console.log($scope.image);
