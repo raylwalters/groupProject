2553222c20d47fc71704c88ef396e89ca3774cb1
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-11 13:12:12 -0400

    able to get object in console, attempting to access api info for uri and generate playlist according to user input

--- js/controllers/musicCtrl.js
+++ js/controllers/musicCtrl.js
@@ -1,18 +1,70 @@
 var app = angular.module('entertainApp');
 
-app.controller('musicCtrl', function($scope, musicFactory){
-  musicFactory.data().then(function(response){
-    console.log(response);
-  })
+app.controller('musicCtrl', function($scope){
+
+
+
+// find template and compile it
+var templateSource = document.getElementById('results-template').innerHTML,
+    template = Handlebars.compile(templateSource),
+    // resultsPlaceholder = document.getElementById('results'),
+    playingCssClass = 'playing',
+    audioObject = null;
+
+
+var fetchTracks = function (albumId, callback) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId,
+        success: function (response) {
+            callback(response);
+            console.log(response);
+        }
+    });
+};
+
+var searchAlbums = function (query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'playlist'
+        },
+        success: function (response) {
+            $scope.musicCall = response.playlists.items.uri;
+            console.log(response);
+            console.log($scope.musicCall);
+        }
+    });
+};
+
+results.addEventListener('click', function(e) {
+    var target = e.target;
+    if (target !== null && target.classList.contains('cover')) {
+        if (target.classList.contains(playingCssClass)) {
+            audioObject.pause();
+        } else {
+            if (audioObject) {
+                audioObject.pause();
+            }
+            fetchTracks(target.getAttribute('data-album-id'), function(data) {
+                audioObject = new Audio(data.tracks.items[0].preview_url);
+                audioObject.play();
+                target.classList.add(playingCssClass);
+                audioObject.addEventListener('ended', function() {
+                    target.classList.remove(playingCssClass);
+                });
+                audioObject.addEventListener('pause', function() {
+                    target.classList.remove(playingCssClass);
+               });
+            });
+        }
+    }
+});
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchAlbums(document.getElementById('query').value);
+}, false);
 
 
 });
-
-//
-// app.controller('moviesCtrl',['$scope','movieList', function($scope,movieList){
-// 	movieList.data().then(function(response){
-// 		console.log(response);
-// 		// console.log("hello");
-// 		$scope.movies = response.data.results;
-// 	});
-// }]);
