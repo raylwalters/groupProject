9f5c8e6f702658482a931c1a5c74fbda75274f13
Ray Walters <rlwalters3@yahoo.com>
2016-08-11 11:03:12 -0400

    Pulled books api with images

--- js/services/booksService.js
+++ js/services/booksService.js
@@ -2,10 +2,23 @@
 
 var app = angular.module('entertainApp');
 
-app.factory('booksService',function($http){
-return {
-	data:function() {
-		return $http.get('https://www.googleapis.com/books/v1/volumes?q=search+terms:comedy&key=AIzaSyBYgMe4rhHaREeA8Z38sDKe-_iOJrIo--w');
-	}
-};
-});
+app.factory('bookList', ['$http', function($http){
+    var randomId = {};
+    function passId(bookId){
+        randomId = bookId;
+        console.log(randomId);
+    }
+    function sendData(){
+        return $http({
+            url:'https://www.googleapis.com/books/v1/volumes?q=subject:' + randomId.id + '&key=AIzaSyCriMrONlAaY3CVEJzBBB-oUZLcOCDAVOA',
+            method: 'GET'
+        }).then(function(response){
+            console.log(response.data.items);
+            return response.data.items;
+        });
+    }
+        return {
+            passId: passId,
+            sendData: sendData
+        };
+}]);
