22fa222088f6dad73ff18d2383c96d6b8f3b2ed4
Ray Walters <rlwalters3@yahoo.com>
2016-08-11 09:29:55 -0400

    fix merge conflicts

--- js/controllers/moviesCtrl.js
+++ js/controllers/moviesCtrl.js
@@ -4,20 +4,6 @@ var app = angular.module('entertainApp');
 
 app.controller('moviesCtrl',['$scope','movieList', function($scope,movieList){
 
-$scope.saveId = function(info){
-        var movieId = info.id;
-        movieList.data(movieId);
-        console.log(movieId);
-    }
-}]);
-
-app.controller('idCtrl',['$scope','movieList', function($scope,movieList){
-        movieList.data().then(function(response){
-            console.log(response);
-            $scope.movies = response.data.results;
-        });
- }]);
-
 	$scope.saveId = function(info){
 		var movieId = info;
 		console.log($scope.movieId);
--- js/services/moviesService.js
+++ js/services/moviesService.js
@@ -4,14 +4,6 @@ var app = angular.module('entertainApp');
 
 app.factory('movieList',['$http', function($http){
 
-	return{
-		data: function(movieId){
-			console.log(movieId);
-			return $http.get("http://api.themoviedb.org/3/discover/movie?api_key=f26d8020ab7996e8ae35fa0d465c6732&with_genres=12" + movieId);
-		}
-	};
-
-
 	var randomId = {};
 
 	function passId(movieId){
