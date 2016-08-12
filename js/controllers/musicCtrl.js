0e493a4baac673f58615ac22f6ed15ac351f4cb6
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-12 11:30:57 -0400

    commit before working to put code in angular

--- js/controllers/musicCtrl.js
+++ js/controllers/musicCtrl.js
@@ -5,24 +5,28 @@ app.controller('musicCtrl', function($scope){
 
 
 // find template and compile it
-var templateSource = document.getElementById('results-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    // resultsPlaceholder = document.getElementById('results'),
-    playingCssClass = 'playing',
-    audioObject = null;
+// var templateSource = document.getElementById('results-template').innerHTML,
+//     template = Handlebars.compile(templateSource),
+//     // resultsPlaceholder = document.getElementById('results'),
+//     playingCssClass = 'playing',
+//     audioObject = null;
     // $scope.resultsPlaceholder = '';
+//
+// var fetchTracks = function (albumId, callback) {
+//     $.ajax({
+//         url: 'https://api.spotify.com/v1/albums/' + albumId,
+//         success: function (response) {
+//             callback(response);
+//             console.log(response);
+//         }
+//     });
+// };
 
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-            console.log(response);
-        }
-    });
-};
+
 
 var searchAlbums = function (query) {
+
+
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
         data: {
@@ -32,46 +36,36 @@ var searchAlbums = function (query) {
         success: function (response) {
             // resultsPlaceholder.innerHTML = template(response);
             $scope.musicCall = response.playlists.items;
-            // $scope.songz = response.playlists.items.external_url;
-
-            //
-            // $scope.playSongs = "https://embed.spotify.com/?uri=";
-
-            // $scope.getIframeSrc = function (songz) {
-            //     return 'https://embed.spotify.com/?uri=' + songz;
-            //   };
-
-// $scope.player = '<iframe src="https://embed.spotify.com/?uri=" + $scope.musicCall +  'width="300" height="380"'></iframe>'
-
             console.log(response);
             console.log($scope.musicCall);
         }
     });
+
 };
 
-results.addEventListener('click', function(e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function(data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function() {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function() {
-                    target.classList.remove(playingCssClass);
-               });
-            });
-        }
-    }
-});
+// results.addEventListener('click', function(e) {
+//     var target = e.target;
+//     if (target !== null && target.classList.contains('cover')) {
+//         if (target.classList.contains(playingCssClass)) {
+//             audioObject.pause();
+//         } else {
+//             if (audioObject) {
+//                 audioObject.pause();
+//             }
+//             fetchTracks(target.getAttribute('data-album-id'), function(data) {
+//                 audioObject = new Audio(data.tracks.items[0].preview_url);
+//                 audioObject.play();
+//                 target.classList.add(playingCssClass);
+//                 audioObject.addEventListener('ended', function() {
+//                     target.classList.remove(playingCssClass);
+//                 });
+//                 audioObject.addEventListener('pause', function() {
+//                     target.classList.remove(playingCssClass);
+//                });
+//             });
+//         }
+//     }
+// });
 
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
