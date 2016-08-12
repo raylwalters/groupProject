eed36fa49207f1301bf757e4861ab0fec559ed11
Katie Golicz <goliczk@gmail.com>
2016-08-12 15:40:48 -0400

    updated with image links

--- js/controllers/booksCtrl.js
+++ js/controllers/booksCtrl.js
@@ -3,43 +3,43 @@
 var app = angular.module('entertainApp');
 
 app.controller('booksCtrl', ['$scope', 'bookList', function($scope, bookList) {
-    $scope.saveId = function(info) {
-	    var bookId = info;
-	    console.log($scope.bookId);
-	    bookList.passId(bookId);
-	    console.log(bookId);
-	    getUrl();
-    };
-    function getUrl() {
-        bookList.sendData().then(function(response){
-            $scope.books = response;
-            //$scope.image = ' ' ;//stuff;
-        });
+    $scope.saveId = function(info) {
+        var bookId = info;
+        console.log($scope.bookId);
+        bookList.passId(bookId);
+        console.log(bookId);
+        getUrl();
+    };
+    function getUrl() {
+        bookList.sendData().then(function(response){
+            $scope.books = response;
+			$scope.links = response.items.accessInfo.webReaderLink;
+            //$scope.image = ' ' ;//stuff;
+        });
 }
-    $scope.info = {
-        model:null,
-        availableOptions: [
-        {id: 'Action', name: 'Action'},
-        {id:'Adventure', name: 'Adventure'},
-        {id:'Biography', name: 'Biography'},
-        {id:'Comedy', name: 'Comedy'},
-        {id:'Crime', name: 'Crime'},
-        {id:'Family', name: 'Family'},
-        {id:'Fantasy', name: 'Fantasy'},
-        {id:'Fiction', name: 'Fiction'},
-        {id:'History', name: 'History'},
-        {id:'Horror', name: 'Horror'},
-        {id:'Music', name: 'Music'},
-        {id:'Mystery', name: 'Mystery'},
-        {id:'Romance', name: 'Romance'},
-        {id:'Science Fiction', name: 'Science Fiction'},
-        {id:'Thriller', name: 'Thriller'},
-        {id:'War', name: 'War'},
-        {id:'Western', name: 'Western'}
-        
-        ]
-	};
+    $scope.info = {
+        model:null,
+        availableOptions: [
+        {id: 'Action', name: 'Action'},
+        {id:'Adventure', name: 'Adventure'},
+        {id:'Biography', name: 'Biography'},
+        {id:'Comedy', name: 'Comedy'},
+        {id:'Crime', name: 'Crime'},
+        {id:'Family', name: 'Family'},
+        {id:'Fantasy', name: 'Fantasy'},
+        {id:'Fiction', name: 'Fiction'},
+        {id:'History', name: 'History'},
+        {id:'Horror', name: 'Horror'},
+        {id:'Music', name: 'Music'},
+        {id:'Mystery', name: 'Mystery'},
+        {id:'Romance', name: 'Romance'},
+        {id:'Science Fiction', name: 'Science Fiction'},
+        {id:'Thriller', name: 'Thriller'},
+        {id:'War', name: 'War'},
+        {id:'Western', name: 'Western'}
+        
+        ]
+    };
 
 
 }]);
-
--- js/services/booksService.js
+++ js/services/booksService.js
@@ -3,22 +3,22 @@
 var app = angular.module('entertainApp');
 
 app.factory('bookList', ['$http', function($http){
-    var randomId = {};
-    function passId(bookId){
-        randomId = bookId;
-        console.log(randomId);lkj
-    }
-    function sendData(){
-        return $http({
-            url:'https://www.googleapis.com/books/v1/volumes?q=subject:' + randomId.id + '&key=AIzaSyCriMrONlAaY3CVEJzBBB-oUZLcOCDAVOA',
-            method: 'GET'
-        }).then(function(response){
-            console.log(response.data.items);
-            return response.data.items;
-        });
-    }
-        return {
-            passId: passId,
-            sendData: sendData
-        };
+    var randomId = {};
+    function passId(bookId){
+        randomId = bookId;
+        console.log(randomId);
+    }
+    function sendData(){
+        return $http({
+            url:'https://www.googleapis.com/books/v1/volumes?q=subject:' + randomId.id + '&key=AIzaSyCriMrONlAaY3CVEJzBBB-oUZLcOCDAVOA',
+            method: 'GET'
+        }).then(function(response){
+            console.log(response.data.items);
+            return response.data.items;
+        });
+    }
+        return {
+            passId: passId,
+            sendData: sendData
+        };
 }]);
