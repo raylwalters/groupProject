113a55bcd5b98c2ce684a37cb6ecd3a07d0839b6
Ray Walters <rlwalters3@yahoo.com>
2016-08-12 23:15:17 -0400

    Put movies, music, books on home partial

--- js/services/mainServices.js
+++ js/services/mainServices.js
@@ -1,5 +1,25 @@
 var app = angular.module('entertainApp');
 
-app.factory('mainService',['$scope', function($scope){
+app.factory('mainMovies',['$http', function($http){
+	return{
+		data: function(){
+			return $http.get("http://api.themoviedb.org/3/discover/movie?api_key=f26d8020ab7996e8ae35fa0d465c6732&with_genres=28");
+			}
+		};
+}]);
 
+app.factory('mainBooks',['$http', function($http){
+	return{
+		data: function(){
+			return $http.get("https://www.googleapis.com/books/v1/volumes?q=subject:Action&key=AIzaSyCriMrONlAaY3CVEJzBBB-oUZLcOCDAVOA");
+			}
+		};
+}]);
+
+app.factory('mainMusic',['$http', function($http){
+	return{
+		data: function(){
+			return $http.get("https://api.spotify.com/v1/search?q=usher&type=artist");
+			}
+		};
 }]);
