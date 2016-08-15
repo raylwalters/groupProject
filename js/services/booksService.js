b6dad95f2bd65639750b4c125976e8237729406b
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-15 14:51:19 -0400

    book styling changes

--- js/services/booksService.js
+++ js/services/booksService.js
@@ -10,7 +10,7 @@ app.factory('bookList', ['$http', function($http){
     }
     function sendData(){
         return $http({
-            url:'https://www.googleapis.com/books/v1/volumes?q=subject:' + randomId.id + '&key=xxxxxxxxx',
+            url:'https://www.googleapis.com/books/v1/volumes?q=subject:' + randomId.id + '&key=AIzaSyCriMrONlAaY3CVEJzBBB-oUZLcOCDAVOA',
             method: 'GET'
         }).then(function(response){
             console.log(response.data.items);
