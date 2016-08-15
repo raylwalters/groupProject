6960b7e22dd074d810d17d87bdf1eba91716d9f4
goliczk <goliczk@gmail.com>
2016-08-15 10:02:19 -0400

    Update mainServices.js

--- js/services/mainServices.js
+++ js/services/mainServices.js
@@ -3,7 +3,7 @@ var app = angular.module('entertainApp');
 app.factory('mainMovies',['$http', function($http){
 	return{
 		data: function(){
-			return $http.get("http://api.themoviedb.org/3/discover/movie?api_key=f26d8020ab7996e8ae35fa0d465c6732&with_genres=28");
+			return $http.get("http://api.themoviedb.org/3/discover/movie?api_key=xxxxxxxxxx&with_genres=28");
 			}
 		};
 }]);
