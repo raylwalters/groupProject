5cfd2fe17aa72ab7c22e4e6d0ede42af8684de9a
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-15 10:24:03 -0400

    changes from over the weekend - need to delete slick-master, i was trying to see about adding carousel plugin

---
+++ script.js
@@ -0,0 +1,15 @@
+//SCRIPT FOR CAROUSEL PLUGIN SLICK
+// $(document).on('ready', function() {
+//   $(".center").slick({
+//     dots: true,
+//     infinite: true,
+//     centerMode: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
+// });
+
+//SCRIPT FOR LETTERING PLUGIN
+$(document).ready(function(){
+  $("#head").lettering();
+});
