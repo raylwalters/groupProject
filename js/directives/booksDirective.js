62089512ec8ec488ae1a6e5e0cad4cfe2f79ebc8
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-16 14:42:47 -0400

    moved copyright to center

--- js/directives/booksDirective.js
+++ js/directives/booksDirective.js
@@ -1,11 +1,11 @@
-'use strict';
-
-var app = angular.module('entertainApp');
-
-app.directive('booksDir',function(){
-	return{
-		restrict: 'AE',
-		templateUrl: 'partials/books.html',
-		replace: false
-	};
-});
+// 'use strict';
+//
+// var app = angular.module('entertainApp');
+//
+// app.directive('booksDir',function(){
+// 	return{
+// 		restrict: 'AE',
+// 		templateUrl: 'partials/books.html',
+// 		replace: false
+// 	};
+// });
--- js/directives/moviesDirective.js
+++ js/directives/moviesDirective.js
@@ -1,11 +1,11 @@
-'use strict';
-
-var app = angular.module('entertainApp');
-
-app.directive('moviesDir',function(){
-	return{
-		restrict: 'AE',
-		templateUrl: 'partials/movies.html',
-		replace: false
-	};
-});
+// 'use strict';
+//
+// var app = angular.module('entertainApp');
+//
+// app.directive('moviesDir',function(){
+// 	return{
+// 		restrict: 'AE',
+// 		templateUrl: 'partials/movies.html',
+// 		replace: false
+// 	};
+// });
