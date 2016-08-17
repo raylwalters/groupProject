3e7404275af04722fe364f10c55021f276bd8c8f
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-17 11:26:21 -0400

    styling changes on home.html

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
-  var shine = new Shine(document.getElementById('shineMovie'));
-  window.addEventListener('mousemove', function(event) {
-    shine.light.position.x = event.clientX;
-    shine.light.position.y = event.clientY;
-    shine.draw();
-  }, false);
+// $(".headlink")
 
-  var shine = new Shine(document.getElementById('shineMusic'));
-  window.addEventListener('mousemove', function(event) {
-    shine.light.position.x = event.clientX;
-    shine.light.position.y = event.clientY;
-    shine.draw();
-  }, false);
+  // var shine = new Shine(document.getElementById('shineMovie'));
+  // window.addEventListener('mousemove', function(event) {
+  //   shine.light.position.x = event.clientX;
+  //   shine.light.position.y = event.clientY;
+  //   shine.draw();
+  // }, false);
+  //
+  // var shine = new Shine(document.getElementById('shineMusic'));
+  // window.addEventListener('mousemove', function(event) {
+  //   shine.light.position.x = event.clientX;
+  //   shine.light.position.y = event.clientY;
+  //   shine.draw();
+  // }, false);
+  //
+  // var shine = new Shine(document.getElementById('shineBooks'));
+  // window.addEventListener('mousemove', function(event) {
+  //   shine.light.position.x = event.clientX;
+  //   shine.light.position.y = event.clientY;
+  //   shine.draw();
+  // }, false);
 
-  var shine = new Shine(document.getElementById('shineBooks'));
-  window.addEventListener('mousemove', function(event) {
-    shine.light.position.x = event.clientX;
-    shine.light.position.y = event.clientY;
-    shine.draw();
-  }, false);
+
 
 
 
